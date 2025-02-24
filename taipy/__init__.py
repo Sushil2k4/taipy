5d0ee817ab96932921065516c8875aee1b365ab8
Sushil Kumar Mishra <131292329+Sushil2k4@users.noreply.github.com>
2025-02-25 00:06:09 +0530

    Update __init__.py

--- taipy/__init__.py
+++ taipy/__init__.py
@@ -1,37 +1,36 @@
 # Copyright 2021-2024 Avaiga Private Limited
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
 #        http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations under the License.
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
 
 from importlib.util import find_spec
+from importlib import import_module
 
+# Check if "taipy" package exists
 if find_spec("taipy"):
-    if find_spec("taipy.common") and find_spec("taipy.common.config"):
-        from taipy.common.config._init import *
+    submodules = [
+        "common.config._init",
+        "gui._init",
+        "core._init",
+        "rest._init",
+        "gui_core._init",
+        "enterprise._init",
+        "designer._init",
+    ]
 
-    if find_spec("taipy.gui"):
-        from taipy.gui._init import *
+    # Dynamically import available submodules
+    for submodule in submodules:
+        module_path = f"taipy.{submodule}"
+        if find_spec(module_path):
+            import_module(module_path)
 
-    if find_spec("taipy.core"):
-        from taipy.core._init import *
-
-    if find_spec("taipy.rest"):
-        from taipy.rest._init import *
-
-    if find_spec("taipy.gui_core"):
-        from taipy.gui_core._init import *
-
-    if find_spec("taipy.enterprise"):
-        from taipy.enterprise._init import *
-
-    if find_spec("taipy.designer"):
-        from taipy.designer._init import *
-
+    # Import taipy._run if available
     if find_spec("taipy._run"):
         from taipy._run import _run as run
